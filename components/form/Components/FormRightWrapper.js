7c86b6bca0cd49107fb3ce33b2da830deca1b2d6
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-03-22 14:57:08 +0530

    Update FormRightWrapper.js

--- components/form/Components/FormRightWrapper.js
+++ components/form/Components/FormRightWrapper.js
@@ -7,6 +7,18 @@ import { create as IPFSHTTPClient } from 'ipfs-http-client';
 const Moralis = require("moralis").default;
 require('dotenv').config();
 
+// const projectId = process.env.NEXT_PUBLIC_IPFS_ID
+// const projectSecret = process.env.NEXT_PUBLIC_IPFS_KEY
+// const auth = 'Basic ' + Buffer.from(projectId + ":" + projectSecret).toString('base64')
+
+// const client = IPFSHTTPClient({
+//   host:'ipfs.infura.io',
+//   port:5001,
+//   protocol: 'https',
+//   headers: {
+//     authorization: auth
+//   }
+// })
 
 const FormRightWrapper = () => {
   const Handler = useContext(FormState);
@@ -18,7 +30,7 @@ const FormRightWrapper = () => {
     console.log(process.env.MORALIS_API_KEY);
     e.preventDefault();
     await Moralis.start({
-      apiKey: process.env.MORALIS_KEY,
+      apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjVjNTQ1MWVlLWFjNTctNGU1Mi05Y2EzLWYzMTRlMjkzNWQxYyIsIm9yZ0lkIjoiMzc1MTM4IiwidXNlcklkIjoiMzg1NTA5IiwidHlwZUlkIjoiMGVjNDVmYjEtMzRjYy00MThhLWE3OWMtZTdhOTNjNjVmNTVhIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDY3MTIyMjAsImV4cCI6NDg2MjQ3MjIyMH0.dutPV0T2uxx7WJqyTm5zcJzkvSCRkLER9meGuGITXV4",
     });
     setUploadLoading(true);
     console.log(Handler);
@@ -53,14 +65,16 @@ const FormRightWrapper = () => {
         var file = Handler.image;
         var reader = new FileReader();
         reader.onloadend = async function () {
-          console.log('RESULT', reader.result)
+          // console.log('RESULT', reader.result)
           const b= [{
             path: Handler.image.name,
             content:reader.result,
           }];
+          console.log(b);
           const response = await Moralis.EvmApi.ipfs.uploadFolder({
             abi: b,
           });
+          console.log(response);
           const index = response.jsonResponse[0].path.indexOf('ipfs/');
           console.log(response.jsonResponse[0].path.substring(index+5));
           Handler.setImageUrl(response.jsonResponse[0].path.substring(index+5))
@@ -123,92 +137,104 @@ const FormRightWrapper = () => {
 // onClick={Handler.startCampaign}
 
 const FormRight = styled.div`
-  width:45%;
-`
+  width: 45%;
+
+  @media (max-width: 431px) and (max-height: 933px) {
+    width: 100%;
+  }
+`;
 
 const FormInput = styled.div`
-  display:flex ;
-  flex-direction:column;
-  font-family:'poppins';
-  margin-top:10px ;
-`
+  display: flex;
+  flex-direction: column;
+  font-family: 'poppins';
+  margin-top: 10px;
+`;
 
 const FormRow = styled.div`
   display: flex;
-  justify-content:space-between;
-  width:100% ;
-`
+  justify-content: space-between;
+  width: 100%;
+`;
 
 const Input = styled.input`
-  padding:15px;
-  background-color:${(props) => props.theme.bgDiv} ;
-  color:${(props) => props.theme.color} ;
-  margin-top:4px;
-  border:none ;
-  border-radius:8px ;
-  outline:none;
-  font-size:large;
-  width:100% ;
-`
+  padding: 15px;
+  background-color: ${(props) => props.theme.bgDiv};
+  color: ${(props) => props.theme.color};
+  margin-top: 4px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  font-size: large;
+  width: 100%;
+`;
 
 const RowFirstInput = styled.div`
-  display:flex ;
-  flex-direction:column ;
-  width:45% ;
-`
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+`;
 
 const RowSecondInput = styled.div`
-  display:flex ;
-  flex-direction:column ;
-  width:45% ;
-`
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+  @media (max-width: 431px) and (max-height: 933px) {
+    // width: 100%;
+    margin-top: 1.5rem;
+  }
+`;
 
 const Select = styled.select`
-  padding:15px;
-  background-color:${(props) => props.theme.bgDiv} ;
-  color:${(props) => props.theme.color} ;
-  margin-top:4px;
-  border:none ;
-  border-radius:8px ;
-  outline:none;
-  font-size:large;
-  width:100% ;
-`
+  padding: 15px;
+  background-color: ${(props) => props.theme.bgDiv};
+  color: ${(props) => props.theme.color};
+  margin-top: 4px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  font-size: large;
+  width: 100%;
+`;
 
 const Image = styled.input`
-  background-color:${(props) => props.theme.bgDiv} ;
-  color:${(props) => props.theme.color} ;
-  margin-top:4px;
-  border:none ;
-  border-radius:8px ;
-  outline:none;
-  font-size:large;
-  width:100% ;
+  background-color: ${(props) => props.theme.bgDiv};
+  color: ${(props) => props.theme.color};
+  margin-top: 4px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  font-size: large;
+  width: 100%;
 
   &::-webkit-file-upload-button {
-    padding: 15px ;
-    background-color: ${(props) => props.theme.bgSubDiv} ;
-    color: ${(props) => props.theme.color} ;
-    outline:none ;
-    border:none ;
-    font-weight:bold ;
-  }  
-`
+    padding: 15px;
+    background-color: ${(props) => props.theme.bgSubDiv};
+    color: ${(props) => props.theme.color};
+    outline: none;
+    border: none;
+    font-weight: bold;
+  }
+`;
 
 const Button = styled.button`
   display: flex;
-  justify-content:center;
-  width:100% ;
-  padding:15px ;
-  color:white ;
-  background-color:#00b712 ;
-  background-image:
-      linear-gradient(180deg, #00b712 0%, #5aff15 80%) ;
-  border:none;
-  margin-top:30px ;
+  justify-content: center;
+  width: 100%;
+  padding: 15px;
+  color: white;
+  background-color: #00b712;
+  background-image: linear-gradient(180deg, #00b712 0%, #5aff15 80%);
+  border: none;
+  margin-top: 30px;
   cursor: pointer;
-  font-weight:bold ;
-  font-size:large ;
-`
+  font-weight: bold;
+  font-size: large;
+
+  @media (max-width: 431px) and (max-height: 933px) {
+    margin-left: 5rem;
+    width: 50%;
+  }
+`;
 
 export default FormRightWrapper
