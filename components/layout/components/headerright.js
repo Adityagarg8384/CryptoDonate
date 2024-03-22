5fe7d3d3345ef84b121fed476315474a6eac4e77
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-03-22 15:17:39 +0530

    Update headerright.js

--- components/layout/components/headerright.js
+++ components/layout/components/headerright.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import {App} from "../layout";
 import { useContext } from "react";
 import Wallet from './wallet';
+import { App } from "../layout";
 
 export const Headerright = () => {
-    const Themetoggler=useContext(App);
+    const Themetoggler = useContext(App);
+
     return (
         <Headerwrapper>
             <Wallet />
@@ -12,25 +13,29 @@ export const Headerright = () => {
                 Themecolor
             </Themetoggle>
         </Headerwrapper>
-    )
+    );
 }
 
 const Headerwrapper = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-margin: 20px;
-
-`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0rem;
+`;
 
 const Themetoggle = styled.div`
-background-color: ${(props)=> props.theme.bgDiv};
-color: ${(props)=> props.theme.color};
-height: 100%;
-padding: 10px;
-font-weight: bold;
-cursor: pointer;
-margin: 20px;
-`
+    background-color: ${(props) => props.theme.bgDiv};
+    color: ${(props) => props.theme.color};
+    padding: 0.4rem;
+    font-weight: bold;
+    font-size: 1rem;
+    cursor: pointer;
+    margin: 1rem;
+
+    @media (max-width: 431px) and (max-height: 933px) {
+        font-size: 0.5rem;
+        padding: 0.2rem;
+    }
+`;
 
 export default Headerright;
