c646358f6edf891a4187086ce4fcc4e495bf6813
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-03-22 19:18:15 +0530

    Update Form.js

--- components/form/Form.js
+++ components/form/Form.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import FormLeftWrapper from './Components/FormLeftWrapper'
 import FormRightWrapper from './Components/FormRightWrapper'
 import { createContext, useState } from 'react';
-import {TailSpin} from 'react-loader-spinner';
-import {ethers} from 'ethers';
-import {toast} from 'react-toastify';
-import CampaignFactory from '../../artifacts/Contracts/Campaigns.sol/CampaignFactory.json'
+import { TailSpin } from 'react-loader-spinner';
+import { ethers } from 'ethers';
+import { toast } from 'react-toastify';
+import CampaignFactory from '../../artifacts/Contracts/Campaigns.sol/CampaignFactory';
 
 const FormState = createContext();
 
@@ -34,107 +34,88 @@ const Form = () => {
     const [image, setImage] = useState("");
 
     const ImageHandler = (e) => {
-        // const name= Handler.image.name;
         setImage(e.target.files[0]);
-       
     }
 
     const startCampaign = async (e) => {
-        // e.preventDefault();
-        // const provider = new ethers.providers.Web3Provider(window.ethereum);
-        // const signer = provider.getSigner();
-        // console.log(signer);
+        e.preventDefault();
+        const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL);
+        const signer = provider.getSigner();
         
-        // if(form.campaignTitle === "") {
-        //   toast.warn("Title Field Is Empty");
-        // } else if(form.story === "" ) {
-        //   toast.warn("Story Field Is Empty");
-        // } else if(form.requiredAmount === "") {
-        //   toast.warn("Required Amount Field Is Empty");
-        // } else if(uploaded == false) {
-        //     toast.warn("Files Upload Required")
-        // }
-        // else {        
-        //   setLoading(true);  
-        //   const contract = new ethers.Contract(
-        //     "0x5085bE1EfD79cf63A9fecB0871B238b3037b19AF",
-        //     CampaignFactory.abi,
-        //     signer
-        //   );
-        //     console.log(contract);
+        if (form.campaignTitle === "") {
+            toast.warn("Title Field Is Empty");
+        } else if (form.story === "") {
+            toast.warn("Story Field Is Empty");
+        } else if (form.requiredAmount === "") {
+            toast.warn("Required Amount Field Is Empty");
+        } else if (!uploaded) {
+            toast.warn("Files Upload Required")
+        } else {
+            setLoading(true);
+            const contract = new ethers.Contract(
+                "0x5085bE1EfD79cf63A9fecB0871B238b3037b19AF",
+                CampaignFactory.abi,
+                signer
+            );
 
-        //   const CampaignAmount = ethers.utils.parseEther(form.requiredAmount);
-         
-        //   const campaignData = await contract.createCampaign(
-        //     form.campaignTitle,
-        //     CampaignAmount,
-        //     imageUrl,
-        //     form.category,
-        //     storyUrl
-        //   );
+            const CampaignAmount = ethers.utils.parseEther(form.requiredAmount);
+            
+            const campaignData = await contract.createCampaign(
+                form.campaignTitle,
+                CampaignAmount,
+                imageUrl,
+                form.category,
+                storyUrl
+            );
 
-        //   await campaignData.wait();   
-    
-        //   setAddress(campaignData.to);
+            await campaignData.wait();
+
+            setAddress(campaignData.to);
         }
     }
 
-  return (
-      <FormState.Provider value={{form, setForm, image, setImage, ImageHandler, FormHandler, setImageUrl, setStoryUrl, startCampaign, setUploaded}} >
-    <FormWrapper>
-        <FormMain>
-            {loading == true ?
-                address == "" ?
-                    <Spinner>
-                        <TailSpin height={60} />
-                    </Spinner> :
-                <Address>
-                    <h1>Campagin Started Sucessfully!</h1>
-                    <h1>{address}</h1>
-                    <Button>
-                        Go To Campaign
-                    </Button>
-                </Address>
-                :
-                    <FormInputsWrapper>
-                        <FormLeftWrapper />
-                        <FormRightWrapper />
-                    </FormInputsWrapper>               
-            }
-        </FormMain>
-    </FormWrapper>
-    </FormState.Provider>
-  )
+    return (
+        <FormState.Provider value={{ form, setForm, image, setImage, ImageHandler, FormHandler, setImageUrl, setStoryUrl, startCampaign, setUploaded }} >
+            <FormWrapper>
+                <FormMain>
+                    {loading ?
+                        address === "" ?
+                            <Spinner>
+                                <TailSpin height={60} />
+                            </Spinner> :
+                            <Address>
+                                <h1>Campagin Started Sucessfully!</h1>
+                                <h1>{address}</h1>
+                                <Button>
+                                    Go To Campaign
+                                </Button>
+                            </Address>
+                        :
+                        <FormInputsWrapper>
+                            <FormLeftWrapper />
+                            <FormRightWrapper />
+                        </FormInputsWrapper>
+                    }
+                </FormMain>
+            </FormWrapper>
+        </FormState.Provider>
+    )
 }
 
 const FormWrapper = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
-
-    @media (max-width: 431px) and (max-height: 933px) {
-        height: 100vh;
-        align-items: center;
-    }
 `;
 
 const FormMain = styled.div`
     width: 80%;
-
-    @media (max-width: 431px) and (max-height: 933px) {
-        width: 90%;
-    }
 `;
 
 const FormInputsWrapper = styled.div`
     display: flex;
     justify-content: space-between;
     margin-top: 45px;
-
-    @media (max-width: 431px) and (max-height: 933px) {
-        flex-direction: column;
-        align-items: center;
-    }
 `;
 
 const Spinner = styled.div`
@@ -168,11 +149,7 @@ const Button = styled.button`
     cursor: pointer;
     font-weight: bold;
     font-size: large;
-
-    @media (max-width: 431px) and (max-height: 933px) {
-        width: 50%;
-    }
 `;
 
 export default Form;
-export {FormState};
+export { FormState };
